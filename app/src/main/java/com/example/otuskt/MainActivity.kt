969fa5bd55ed2efb9ef3d68c07684e55fb0b7a00
RomanRazdorov <danka.kuzka@mail.ru>
2023-10-16 23:22:28 +0300

    Added URL Download

--- app/src/main/java/com/example/otuskt/MainActivity.kt
+++ app/src/main/java/com/example/otuskt/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.otuskt
 
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.EditText
 import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.lifecycle.coroutineScope
@@ -15,6 +18,8 @@ import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.GlobalContext.startKoin
 import kotlin.Error
 
+
+
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private val viewModel: MainViewModel by viewModels()
@@ -22,6 +27,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val editTextImageUrl = binding.urlField;
+
+
+        binding.btnNext.setOnClickListener()
+        {
+            btnListener(editTextImageUrl);
+        }
 
         lifecycle.coroutineScope.launch {
 
@@ -31,6 +44,7 @@ class MainActivity : AppCompatActivity() {
                     renderState(state)
                 }
         }
+
         startKoin {
             androidContext(this@MainActivity)
             modules(appModule)
@@ -65,6 +79,51 @@ class MainActivity : AppCompatActivity() {
     private fun renderError() {
         binding.swipeLayout.isRefreshing = false
         Toast.makeText(this, getString(R.string.error_text), Toast.LENGTH_SHORT).show()
+    }
+
+    private fun btnListener(editTextImageUrl: EditText)
+    {
+
+        val imageUrl = editTextImageUrl.text.toString()
+        if (imageUrl.isNotEmpty()) {
+            Toast.makeText(this, "гуд", Toast.LENGTH_SHORT).show()
+
+            val networkThread = Thread {
+                try {
+                    val bitmap = Picasso.get().load(imageUrl).get()
+
+                    val diskThread = Thread {
+                        saveImageToInternalStorage(bitmap, fileName = "image.jpg")
+                    }
+                    diskThread.start()
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            }
+            networkThread.start()
+        } else {
+            Toast.makeText(this, "Введите URL изображения", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun saveImageToInternalStorage(bitmap: Bitmap, fileName: String) {
+        val context = this
+
+        try {
+            // Открываем или создаем файл для сохранения изображения во внутренней памяти
+            val fileOutputStream = context.openFileOutput(fileName, MODE_PRIVATE)
+
+            // Сохраняем изображение в формате JPEG с качеством 100 (максимальное качество)
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fileOutputStream)
+
+            // Закрываем поток после записи
+            fileOutputStream.close()
+
+            Log.i("SafeImage","Изображение успешно сохранено")
+        } catch (e: Exception) {
+            e.printStackTrace()
+            Log.e("SafeImage","Ошибка при сохранении изображения")
+        }
     }
 
 
